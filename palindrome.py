0b0bd3caf3810ab477a34e21926d9e993834e74e
Ano Jumisa <anojumisa@gmx.de>
2024-05-29 12:13:19 +0700

    Solved palindrome.py 2nd attempt

--- palindrome.py
+++ palindrome.py
@@ -18,15 +18,18 @@ def is_palindrome(s: str) -> bool:
     - is_palindrome("racecar") should return True
     - is_palindrome("hello") should return False
     """
-    s = s.lower()  
     left, right = 0, len(s) - 1
     while left < right:
-        if s[left] != s[right]:
+        while left < right and not (s[left].isalpha() or s[left].isdigit()):
+            left += 1
+        while left < right and not (s[right].isalpha() or s[right].isdigit()):
+            right -= 1
+        if s[left].lower() != s[right].lower():
             return False
         left += 1
         right -= 1
     return True
-print(is_palindrome("HEllo"))
+print(is_palindrome("A man, a plan, a canal, Panama"))
 
 # You can test your function with print statements below
 # Example:
